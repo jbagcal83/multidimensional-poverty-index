c3dbf51736ec376400f89f5b0c40f0b1edfdbb52
Jo <37987471+jbagcal83@users.noreply.github.com>
2018-10-05 01:03:01 -0400

    Update MPI_means_stdevs.R
    corrected some errors in the code

--- MPI_means_stdevs.R
+++ MPI_means_stdevs.R
@@ -1,16 +1,16 @@
 # mean and stdev of poverty data
 
 mean_MPIUrban <- mean(poverty$`MPI Urban`)
-stdev_MPIUrban <- stdev(poverty$`Headcount Ratio Urban`)
+stdev_MPIUrban <- sd(poverty$`Headcount Ratio Urban`)
 
 mean_MPIRural <- mean(poverty$`MPI Rural`)
-stdev_MPIRural <- stdev(poverty$`MPI Rural`)
+stdev_MPIRural <- sd(poverty$`MPI Rural`)
 
 mean_int_Urban <- mean(poverty$`Intensity of Deprivation Urban`)
-stdev_int_urban <- mean(poverty$`Intensity of Deprivation Urban`)
+stdev_int_urban <- sd(poverty$`Intensity of Deprivation Urban`)
 
 mean_int_rural <- mean(poverty$`Intensity of Deprivation Rural`)
-stdev_int_rural <- stdev(poverty$`Intensity of Deprivation Rural`)
+stdev_int_rural <- sd(poverty$`Intensity of Deprivation Rural`)
 
 mean_MPIUrban
 stdev_MPIUrban
