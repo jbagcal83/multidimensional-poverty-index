19b8152c81c3822e78be8ed9c57c1470b0cbf8be
Jo <37987471+jbagcal83@users.noreply.github.com>
2018-10-05 11:21:53 -0400

    Update MPI.R
    improved plots, added another plot, "Intensity of Deprivation vs. MPI"

--- MPI.R
+++ MPI.R
@@ -1,22 +1,31 @@
 # dataset obtained on kaggle.com
 # headcount ratio is the percentage of people in poverty
 # (multidimensional poverty) per country
+# MPI = 
 
 
 poverty <- read_csv("R/MPI_national.csv")
 
 p <- ggplot(poverty, aes(x = `Headcount Ratio Urban`, 
                          y = `Intensity of Deprivation Urban`)) + 
-  geom_point(aes(color='Headcount Ratio Urban'))
+  geom_point(aes(color=`Headcount Ratio Urban`))
 
 p2 <- p + geom_point(aes(x = `Headcount Ratio Rural`,
-                          y = `Intensity of Deprivation Rural`)) +
+                          y = `Intensity of Deprivation Rural`),
+                     color = "red") +
                          xlab("Headcount Ratio") +
                           ylab("Intensity of Deprivation") + geom_smooth(method="lm") +
-  ggtitle("Urban and Rural Headcount Ratio vs Deprivation Intensity")
+  ggtitle("Intensity of Deprivation vs. Headcount Ratio")
 
 p2
 
-# headcount ratio = people in actual poverty in each country
-# divided by total population in each country
+p3 <- ggplot(poverty, aes(x = `MPI Urban`, 
+                          y = `Intensity of Deprivation Urban`)) + 
+  geom_point(aes(color = `MPI Urban`))
+
+p4 <- p3 + geom_point(aes(x = `MPI Rural`,
+                          y = `Intensity of Deprivation Rural`),
+                      color = "red") +
+  xlab("MPI") + ylab("Intensity of Deprivation") +
+  ggtitle("Intensity of Deprivation vs. MPI")
 
