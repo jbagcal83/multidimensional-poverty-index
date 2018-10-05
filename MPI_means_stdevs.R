0ceeddc6ea9635d57c77f18db868e59689b7431c
Jo <37987471+jbagcal83@users.noreply.github.com>
2018-10-05 00:04:19 -0400

    Add files via upload
    Means and standard deviations of multidimensional poverty indices for rural areas, urban areas, and mean and standard deviations for intensity of deprivation, both rural and urban areas.

---
+++ MPI_means_stdevs.R
@@ -0,0 +1,22 @@
+# mean and stdev of poverty data
+
+mean_MPIUrban <- mean(poverty$`MPI Urban`)
+stdev_MPIUrban <- stdev(poverty$`Headcount Ratio Urban`)
+
+mean_MPIRural <- mean(poverty$`MPI Rural`)
+stdev_MPIRural <- stdev(poverty$`MPI Rural`)
+
+mean_int_Urban <- mean(poverty$`Intensity of Deprivation Urban`)
+stdev_int_urban <- mean(poverty$`Intensity of Deprivation Urban`)
+
+mean_int_rural <- mean(poverty$`Intensity of Deprivation Rural`)
+stdev_int_rural <- stdev(poverty$`Intensity of Deprivation Rural`)
+
+mean_MPIUrban
+stdev_MPIUrban
+mean_MPIRural
+stdev_MPIRural
+mean_int_Urban
+stdev_int_urban
+mean_int_rural
+stdev_int_rural
